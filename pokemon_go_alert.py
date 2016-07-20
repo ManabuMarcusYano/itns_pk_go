f4dc4c197a0de6092a0582b0f6742109185a9ece
Manabu_Yano <markn0109@gmail.com>
2016-07-20 16:09:14 +0900

    for cron

--- pokemon_go_alert.py
+++ pokemon_go_alert.py
@@ -9,7 +9,6 @@ import tweet as tw
 url = 'https://itunes.apple.com/search?term=pokemon+go&country=jp&media=software&entity=software&lang=ja_jp'
 
 def func():
-    while True:
         try:
             r = urllib2.urlopen(url)
             root = json.loads(r.read())
@@ -21,6 +20,5 @@ def func():
             print "has not been released yet in Japan"
         finally:
             r.close()
-        time.sleep(30.0)
 
 func()
